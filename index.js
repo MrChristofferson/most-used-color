14a7ebf85152b12368ceb30a24631f7fcef5bcc9
Chris Vandenberg <chrisvan0848@gmail.com>
2016-08-21 15:47:21 -0700

    refactor

--- index.js
+++ index.js
@@ -1,71 +1,46 @@
-'use strict';
+'use strict'
 
-var getter = require('pixel-getter');
+var getter = require('pixel-getter')
 
 module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+	if (typeof input !== 'string') throw new TypeError(`Expected a string, got ${typeof input}`)
 
-	this.getAllPixels = input => {
-	    return new Promise((resolve, reject) => {
-	        getter.get(`${input}`, (err, pixels) => {
-	            if (err) {
-	                reject(err);
-	            }
-	            resolve(pixels[0]);
-	        });
-	    });
-	}
+	return new Promise((resolve, reject) => {
 
-	this.getPixelData = pixels => {
-	    var colors = new Array();
+		  getter.get(`${input}`, (err, pixels) => {
 
-	    for (var i = 0; i < pixels.length; i++) {
+		    if (err) console.log(err)
 
-	        var p = pixels[i];
+				var colors = new Array()
 
-	        if (!colors[p.r]) colors[p.r] = new Array();
+				for (var i = 0; i < pixels.length; i++) {
+						var p = pixels[i]
+						if (!colors[p.r]) colors[p.r] = new Array()
+						if (!colors[p.r][p.g]) colors[p.r][p.g] = new Array()
+						if (!colors[p.r][p.g][p.b]) {
+								var color = `${p.r}, ${p.g}, ${p.b}`
+								colors[p.r][p.g][p.b] = {color: color, count: 0}
+						}
+						colors[p.r][p.g][p.b].count++
+				}
 
-	        if (!colors[p.r][p.g]) colors[p.r][p.g] = new Array();
+				var max
+				var maxVal = 0
+				for (var r in colors) {
+						for (var g in colors[r]) {
+								for (var b in colors[r][g]) {
+										if (maxVal < colors[r][g][b].count) {
+												maxVal = colors[r][g][b].count
+												max = colors[r][g][b]
+										}
+								}
+						}
+				}
 
-	        if (!colors[p.r][p.g][p.b]) {
-	            var color = `${p.r}, ${p.g}, ${p.b}`;
-	            colors[p.r][p.g][p.b] = {color: color, count: 0};
-	        }
+				resolve(max.color)
 
-	        colors[p.r][p.g][p.b].count++;
+		  })
 
-	    }
+	})
 
-	    var max;
-	    var maxVal = 0;
-	    for (var r in colors) {
-	        for (var g in colors[r]) {
-	            for (var b in colors[r][g]) {
-	                if (maxVal < colors[r][g][b].count) {
-	                    maxVal = colors[r][g][b].count;
-	                    max = colors[r][g][b];
-	                }
-	            }
-	        }
-	    }
-
-	    return new Promise((resolve, reject) => {
-	        resolve({primaryColor: max.color});
-	    });
-	}
-
-	this.run = target => {
-	    return new Promise((resolve, reject) => {
-	        this.getAllPixels(target)
-	            .then(pixels => {
-	                resolve(this.getPixelData(pixels));
-	            });
-	    });
-	}
-
-	opts = opts || {};
-
-	return input + ' & ' + (opts.postfix || 'rainbows');
-};
+}
